f7dc1107498ca29466b6eccaa6f75a0027b66a83
SumaBadugu <sumasrinivas0211@gmail.com>
2025-08-27 22:27:13 +0530

    Wrote code for finding even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,17 @@
+public class Utility {
+    public static void main(String[] args){
+        checkEvenOrOdd(7);
+        suma();
+    }
+    public static checkEvenOrOdd(int n){
+        if(n%2==0){
+            System.out.println("The number is even..");
+        }
+        else{
+            System.out.println("The number is odd..");
+        }
+    }
+    public static suma{
+        System.out.println("I am Suma!!")
+    }
+}
